34d1a271139b3fcf575ae21a36bb29ab5bb209e6
packet <199sd@naver.com>
2018-03-10 21:30:15 +0900

    Modified the source code

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
+#-*- coding: utf-8 -*-
 from flask import Flask, request
-import parser
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get('http://www.sunrint.hs.kr/index.do')
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+menu = soup.select(' dl > dd > p.menu')
+def today_menu():
+    today = menu[0].text
+    print(today)
+    return today
+def tomorrow_menu():
+    tomorrow = menu[1].text
+    return tomorrow
 
 application = Flask(__name__)
 @application.route('/keyboard')
 def keyboard():
     key = '{"type":"buttons", "buttons":["오늘의 급식", "내일의 급식"]}'
     return key
-@application.route('/message')
+@application.route('/message',methods=['POST','GET'])
 def message():
     if request.method == "POST":
-        select = request.form["button"]
-        if select == "오늘의 급식":
-            menu = parser.today_menu()
+        select = request.form['button']
+        if select == '오늘의 급식':
+            menu = today_menu()
             return menu
-        elif select == "내일의 급식":
-            menu = parser.tomorrow_menu()
+        elif select == '내일의 급식':
+            menu = tomorrow_menu()
             return menu
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
